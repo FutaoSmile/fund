81a9dc2ddbebc094f2c66337bf744dfcb4a422cc
futao <futao@mysteel.com>
2022-05-19 11:14:18 +0800

    初始化

---
+++ fund-api/src/main/java/com/futao/fund/api/Api.java
@@ -0,0 +1,8 @@
+package com.futao.fund.api;
+
+/**
+ * @author futaosmile@gmail.com
+ * @date 2022/5/19
+ */
+public class Api {
+}
---
+++ fund-mgt/src/main/java/com/futao/fund/mgt/Mgt.java
@@ -0,0 +1,8 @@
+package com.futao.fund.mgt;
+
+/**
+ * @author futaosmile@gmail.com
+ * @date 2022/5/19
+ */
+public class Mgt {
+}
