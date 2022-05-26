8d30ffec05d3fdc5ae2b28be1fa8ff9bc177fec4
futao <futao@mysteel.com>
2022-05-26 10:50:24 +0800

    构造方法注入

--- fund-provider/src/main/java/com/futao/fund/provider/task/StartTask.java
+++ fund-provider/src/main/java/com/futao/fund/provider/task/StartTask.java
@@ -1,12 +1,12 @@
 package com.futao.fund.provider.task;
 
 import com.alibaba.fastjson.JSON;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -17,11 +17,10 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class StartTask {
 
-
-    @Autowired
-    private RestHighLevelClient restHighLevelClient;
+    private final RestHighLevelClient restHighLevelClient;
 
     @Bean
     public ApplicationRunner es() {
